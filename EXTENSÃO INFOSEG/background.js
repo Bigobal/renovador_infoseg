a4e742813c51c67b466a3e4a73ceafe02596e78a
Oscar <oscar.cordeiro.a@gmail.com>
2024-12-12 17:16:54 -0300

    Ajustes na troca de script.

--- EXTENSÃO INFOSEG/background.js
+++ EXTENSÃO INFOSEG/background.js
@@ -1,15 +1,3 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'saveData') {
-    const data = request.data;
-
-    const blob = new Blob([data], { type: 'text/plain;charset=utf-8' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'dados_extracao.txt';
-    a.click();
-    URL.revokeObjectURL(url);
-
-    sendResponse({ status: 'success' });
-  }
+chrome.runtime.onInstalled.addListener(function() {
+    console.log('Extensão de renovação de sessão instalada.');
 });
